f3407931fc373dac4b4ab3c1f370287e5789b358
fomalhaut18 <69924217+fomalhaut18@users.noreply.github.com>
2020-08-22 10:45:35 -0700

    Add files via upload

--- track_action.py
+++ track_action.py
@@ -1,7 +1,23 @@
 from mixpanel import Mixpanel
+import discord
 
 mp = Mixpanel('6e8340ea62ff79b774da5fdbdcad0b2d')
 
 #Tracks an event, 'makes Transaction'
-def track_action(unique_id):
-    mp.track(unique_id, 'action')
+def set_profile(unique_id, token_name, client):
+    mp.people_set(unique_id, {
+        'balance' : get_balance(unique_id, token_name),
+        'server' : client.get_guild(discord.Guild.id)
+    })
+
+def track_code(unique_id):
+    mp.track(unique_id, 'code')
+
+def track_withdraw(unique_id):
+    mp.track(unique_id, 'withdraw')
+
+def track_send(unique_id):
+    mp.track(unique_id, 'send')
+
+def track_drop(unique_id):
+    mp.track(unique_id, 'drop')
