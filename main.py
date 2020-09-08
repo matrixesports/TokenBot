6f102f2a0b8efb037a7242f407ef33a8b82e479c
JZ <jz@pop-os.localdomain>
2020-09-07 22:08:07 -0700

    wut

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 from Bot_Prod import runBotProd
 from Bot_Staging import runBotStaging
 import os
-import http.server
-import socketserver
 
 ZEET_ENVIRONMENT = os.getenv('ZEET_ENVIRONMENT')
-PORT = 8000
 
-handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), handler) as httpd:
-    print("Server started at localhost:" + str(PORT))
-    httpd.serve_forever()
 print(ZEET_ENVIRONMENT)
 
 
