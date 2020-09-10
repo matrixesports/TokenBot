ed7ce279497b6d2352e41227f1aad1699473daa1
JZ <jz@pop-os.localdomain>
2020-09-09 17:31:33 -0700

    Okeydokey lets see if we can upload the DB file

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ from dotenv import load_dotenv
 import os
 import time
 from track_action import *
+from datetime import date
+from DBBackup import upload_to_aws
+
+print("Main.py running")
+ZEET_ENVIRONMENT = os.getenv('ZEET_ENVIRONMENT')
+
+local_file = "/data/discord-commerce.db"
+bucket_name = "matrixdatabasebackup"
+s3_file_name = str(date.today())
+
+uploaded = upload_to_aws('local_file', 'bucket_name', 's3_file_name')
+
+#if ZEET_ENVIRONMENT == "master":
+    #runBotProd()
+#else:
+    #runBotStaging()
+
+upload_to_aws(local_file, bucket_name, s3_file_name)
+print(s3_file_name + " uploaded")
 
 load_dotenv()
 example_withdraw = "$withdraw eth_address token_name token_count"
