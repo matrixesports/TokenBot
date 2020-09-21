71cbd67367d36329b2848526012522cbbd25bc95
JZ <jz@pop-os.localdomain>
2020-09-21 15:43:23 -0700

    random stuff go away

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from Bot_Prod import runBotProd
 from Bot_Staging import runBotStaging
 import os
-<<<<<<< HEAD
 import time
 from track_action import *
 from datetime import date
@@ -722,24 +721,3 @@ async def on_raw_reaction_add(payload):
             
 if __name__ == '__main__':
     client.run(APIKEY)
-=======
-from datetime import date
-from DBBackup import upload_to_aws
-
-print("Main.py running")
-ZEET_ENVIRONMENT = os.getenv('ZEET_ENVIRONMENT')
-
-local_file = "/data/discord-commerce.db"
-bucket_name = "matrixdatabasebackup"
-s3_file_name = str(date.today())
-
-uploaded = upload_to_aws('local_file', 'bucket_name', 's3_file_name')
-
-if ZEET_ENVIRONMENT == "master":
-    runBotProd()
-else:
-    runBotStaging()
-
-upload_to_aws(local_file, bucket_name, s3_file_name)
-print(s3_file_name + " uploaded")
->>>>>>> 832602bd5a010bcbddc6cc0b540fefa1e22b12f3
