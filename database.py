40fd39a115bf9f6fb1c04002427ecf371072c1c7
JZ <you@example.com>
2020-10-13 22:00:11 -0700

    Yay COMMENTED CODE

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
+#imports the sqlite lib, you'll switch this to whatever postgresql needs
 import sqlite3
 
+#connects to the sqlite DB
 connection = sqlite3.connect("/data/discord-commerce.db")
 cursor = connection.cursor()
 create_table = """CREATE TABLE IF NOT EXISTS balances (
@@ -7,6 +9,7 @@ create_table = """CREATE TABLE IF NOT EXISTS balances (
 );"""
 cursor.execute(create_table)
 connection.commit()
+#adds a column to the database with a new token. This is called during $create_token.
 def add_token(token_name):
     token_name=token_name.lower()
     if token_name in get_token_list():
@@ -15,21 +18,21 @@ def add_token(token_name):
         cursor.execute(
             "ALTER TABLE balances ADD " + token_name + " NOT NULL DEFAULT 0;")
 
-
+#Removes the token from the database. We don't use this.
 def remove_token(token_name):
     cursor.execute(
         "ALTER TABLE balances ADD ? NOT NULL DEFAULT 0;", (token_name, ))
 
-
+#Checks whether a certain user is in the database already. (Checks the Discord User ID)
 def user_exists(unique_id):
     return bool(cursor.execute("SELECT EXISTS(select * from balances where username=?)",(unique_id,)).fetchone()[0])
 
-
+#Adds a user into the database. Uses the Discord User ID as the identifier. 
 def add_user(unique_id):
     cursor.execute("""INSERT into balances (username) VALUES(?)""", (unique_id,))
     connection.commit()
 
-
+#Sets the balance of the user. This is used whenever some one gets (receive, claim drop/code, etc.) tokens, sends tokens, buys an item.
 def set_balance(unique_id, token_name, balance):
     token_name=token_name.lower()
     if user_exists(unique_id) == False:
@@ -38,7 +41,7 @@ def set_balance(unique_id, token_name, balance):
         cursor.execute("update balances set "  + token_name.lower() + " = ? where username = ?",(balance, unique_id))
         connection.commit()
 
-
+#Grabs how many tokens someone has
 def get_balance(user, token_name):
     token_name=token_name.lower()
     if token_name in get_token_list():
@@ -48,7 +51,7 @@ def get_balance(user, token_name):
         return int(balance)
     else:
         return None
-  
+                
 
 
 def get_token_list():
